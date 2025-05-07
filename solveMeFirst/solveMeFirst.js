320573b2d875ebf3896b6042f325a8721b20a4f9
Md. Tarikul Islam <tariikul12@gmail.com>
2025-05-07 22:05:44 +0600

    solve me first

---
+++ solveMeFirst/solveMeFirst.js
@@ -0,0 +1,13 @@
+/**
+ * input: two integers
+ * process: compute the sum
+ * output: return summation
+ */
+
+function solveMeFirst(a, b) {
+    // Hint: Type return a+b below 
+    
+  return a+b;
+  }
+
+  solveMeFirst(2,3)
